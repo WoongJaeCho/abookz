ed9dfee23f1641640d89fe08c2775456f7236b2e
WoongJaeCho <153791208+WoongJaeCho@users.noreply.github.com>
2024-04-01 09:57:19 +0900

    Form

---
+++ abookz/src/main/java/kr/basic/abookz/AbookzApplication.java
@@ -0,0 +1,13 @@
+package kr.basic.abookz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AbookzApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AbookzApplication.class, args);
+    }
+
+}
---
+++ abookz/src/test/java/kr/basic/abookz/AbookzApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.basic.abookz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AbookzApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
