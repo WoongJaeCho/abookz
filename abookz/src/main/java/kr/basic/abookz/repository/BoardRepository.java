24e416cdde9f398d4c6d800f6c421553ef776ae7
LEEjunswer <jun_5949@naver.com>
2024-04-11 18:20:04 +0900

    나의 서재 변경작업 60%완료
    나의 서재 변경작업 60%완료

--- abookz/src/main/java/kr/basic/abookz/repository/BoardRepository.java
+++ abookz/src/main/java/kr/basic/abookz/repository/BoardRepository.java
@@ -6,9 +6,12 @@ import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
+import java.util.List;
+
 public interface BoardRepository extends JpaRepository<BoardEntity, Long> {
   // update BOARD set BOARD_hits = BOARD_hits + 1 where id = ?
   @Modifying
   @Query(value = "update BoardEntity b set b.hits=b.hits+1 where b.id=:id")
   void updateHits(@Param("id") Long id);
+
 }
