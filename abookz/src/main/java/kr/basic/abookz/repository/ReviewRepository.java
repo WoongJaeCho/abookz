12d89870282f4ecd68594b59338b25183a2d48dc
ChoiSeoungH <147977134+ChoiSeoungH@users.noreply.github.com>
2024-04-19 09:26:22 +0900

    Update ReviewRepository.java

--- abookz/src/main/java/kr/basic/abookz/repository/ReviewRepository.java
+++ abookz/src/main/java/kr/basic/abookz/repository/ReviewRepository.java
@@ -19,10 +19,10 @@ public interface ReviewRepository extends JpaRepository<ReviewEntity,Long> {
   @Query("SELECT r FROM ReviewEntity r WHERE r.bookShelf.book.ISBN13 = :ISBN13 AND r.content LIKE %:content%")
   Page<ReviewEntity> findByBookISBN13AndContent(@Param("ISBN13") String ISBN13, @Param("content") String content, Pageable pageable);
 
-  @Query(value = "SELECT r.*, bs.BOOKSHELF_ID AS bs_bookshelf_id, b.*, likes.likeCount FROM REVIEW r " +
-      "INNER JOIN BOOKSHELF bs ON r.BOOKSHELF_ID = bs.BOOKSHELF_ID " +
-      "INNER JOIN BOOK b ON bs.BOOK_ID = b.BOOK_ID " +
-      "LEFT JOIN (SELECT l.REVIEW_ID, COUNT(l.LIKE_ID) AS likeCount FROM REVIEW_LIKE l GROUP BY l.REVIEW_ID) likes ON r.REVIEW_ID = likes.REVIEW_ID " +
+  @Query(value = "SELECT r.*, bs.BOOKSHELF_ID AS bs_bookshelf_id, b.*, likes.likeCount FROM review r " +
+      "INNER JOIN bookshelf bs ON r.BOOKSHELF_ID = bs.BOOKSHELF_ID " +
+      "INNER JOIN book b ON bs.BOOK_ID = b.BOOK_ID " +
+      "LEFT JOIN (SELECT l.REVIEW_ID, COUNT(l.LIKE_ID) AS likeCount FROM review_like l GROUP BY l.REVIEW_ID) likes ON r.REVIEW_ID = likes.REVIEW_ID " +
       "WHERE b.ISBN13 = :ISBN13 AND r.CONTENT LIKE CONCAT('%', :content, '%') " +
       "ORDER BY likes.likeCount DESC", nativeQuery = true)
   Page<ReviewEntity> findByBookISBN13AndContentSortedByLikes(@Param("ISBN13") String ISBN13, @Param("content") String content, Pageable pageable);
