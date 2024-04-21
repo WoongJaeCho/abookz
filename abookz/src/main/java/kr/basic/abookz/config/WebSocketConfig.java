3d0ded4b3bbdd8d758a091927b0dc912ce30a6d1
ChoiSeoungH <inevers93@gmail.com>
2024-04-21 14:22:32 +0900

    update

--- abookz/src/main/java/kr/basic/abookz/config/WebSocketConfig.java
+++ abookz/src/main/java/kr/basic/abookz/config/WebSocketConfig.java
@@ -5,8 +5,6 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.messaging.handler.annotation.MessageMapping;
-import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
